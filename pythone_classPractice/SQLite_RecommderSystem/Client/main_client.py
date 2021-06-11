b6555758582f5d51f75dad0ce8f758c95b399f0b
samh99474 <samh99474@gmail.com>
2021-06-12 02:32:39 +0800

    samh queryMovie

--- pythone_classPractice/SQLite_RecommderSystem/Client/main_client.py
+++ pythone_classPractice/SQLite_RecommderSystem/Client/main_client.py
@@ -2,24 +2,26 @@ from SocketClient.Socket_Client import SocketClient
 from AddUser import AddUser
 from Get_Movie_RS import Get_Movie_RS
 from Rating import Rating
+from QueryMovie import QueryMovie
 
 action_list = {
     "add": AddUser,
     "RS" : Get_Movie_RS,
-    "rating": Rating
+    "rating": Rating,
+    "QueryMovie": QueryMovie
 }
 
 #======================================================================
 def print_menu():
         print()
-        print("add: Add a student's name")
+        print("add: Add a user's name")
         print("RS: Get Movie Recommemdation")
         print("rating: rating a Movie")
+        print("QueryMovie: Query a Movie")
         print("exit: Exit")
         selection = input("Please select: ")
 
         return selection
-
 #================================================================
 
 def main():
